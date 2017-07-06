c03d57186e2bd259e196ccba779b68164949d083
Vijay Bheemineni <bheemineni.vijay@gmail.com>
2017-07-06 12:37:02 -0500

    Commented status command in stop-apache.sh script. Since this command is returning 3 error code and code deploy is failing.

--- server-scripts/stop-apache.sh
+++ server-scripts/stop-apache.sh
@@ -6,7 +6,7 @@ then
   echo "Apache2 service running, so we will stop it."
   sudo systemctl stop apache2.service
   echo "Apache2 service had been stopped."
-  sudo systemctl status apache2.service
+  #sudo systemctl status apache2.service
 else
   echo "Apache2 service already stopped."
 fi
